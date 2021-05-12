0a2d994b2d8fae6ce704e42bc96243ca84d93788
mdshahnawazz <70908310+mdshahnawazz@users.noreply.github.com>
2021-05-12 13:30:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const heading="Shopping website";
+  const description="Do what ever you want to shopping in this website.";
   return (
-    <div id="main"></div>
-  )
-}
+    <div id="main">
+      <h1 data-ns-test="project-name">{heading}</h1>
+      <p data-ns-test="project-description">{description}</p>
+    </div>
+  );
+};
 
 
 export default App;
